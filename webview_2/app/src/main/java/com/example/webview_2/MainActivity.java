855b99afac7b8507ab486f09bec80384e3a9d2e0
chickpow2 <lok121921@gmail.com>
2020-12-09 17:33:28 +0800

    chickpow

--- webview_2/app/src/main/java/com/example/webview_2/MainActivity.java
+++ webview_2/app/src/main/java/com/example/webview_2/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.webview_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -11,7 +13,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//set content view AFTER ABOVE sequence (to avoid crash)
+        this.setContentView(R.layout.activity_main);
         webView = (WebView) findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("http://10.0.2.2:8080/4511Assignment/login.jsp");
